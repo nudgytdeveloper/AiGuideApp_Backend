e31db512b779fd6392ee89a89fb7e3416a6fd323
Chan Thai Thong <thongz0819@live.com>
2025-10-03 14:00:09 +0800

    - resolve import issue

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import {
   InternalServerError,
   NotFound,
   Success,
-} from "./constant/StatusCode"
+} from "./constant/StatusCode.js"
 
 const app = express()
 const PORT = process.env.PORT || 3000
